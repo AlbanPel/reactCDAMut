5f6be4f12e17486baccbefef3012831ab1b3db33
albanPel <alban@pelissier.dev>
2024-03-11 15:15:30 +0100

    switch theme

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import {Container, createTheme, CssBaseline, ThemeProvider} from "@mui/material";
+import {Container, createTheme, CssBaseline, FormControlLabel, Switch, ThemeProvider} from "@mui/material";
 import {dark} from "@mui/material/styles/createPalette";
+import {useState} from "react";
 
 
 export default function App() {
+    const [dark, setDark] = useState(true);
+    const [labelTheme, setLabelTheme] = useState("theme sombre")
+
+    const handleChange = () => {
+        labelTheme === "theme sombre" ? setLabelTheme('Theme Clair') : setLabelTheme('Theme sombre')
+    }
+
+
   return (
-      <ThemeProvider theme={themeDark}>
+      <ThemeProvider theme={dark === true ? themeDark : themeLight}>
           <CssBaseline />
           <Container maxWidth="xl">
+              <FormControlLabel
+                  control={<Switch defaultChecked color="secondary"/> }
+                  value={labelTheme}
+                  label={labelTheme}
+                  onClick={dark === true ? ()=>{setDark(false)} : ()=>{setDark(true)}}
+                  onChange={handleChange}
+                  />
 
 
           </Container >
@@ -33,5 +49,22 @@ const themeDark = createTheme({
     text: {
         primary: "#fff"
     }
+})
 
+const themeLight = createTheme({
+    palette: {
+        background: {
+            paper: '#ced6db',
+            default: "#b1deee"
+        }
+    },
+    primary: {
+        main: '#f0f0f7'
+    },
+    secondary: {
+        main: '#a2a2a2'
+    },
+    text: {
+        primary: "#000000"
+    }
 })
